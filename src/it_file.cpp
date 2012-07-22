4f57eb3b0b0def38307882a3393d6208a43bf9c1
Bogdan Cristea <cristeab@gmail.com>
2012-07-22 22:29:48 +0200

    add input check

--- src/it_file.cpp
+++ src/it_file.cpp
@@ -7,6 +7,10 @@ itpp::it_file file;
 
 int it_file_open(const char *const file_name)
 {
+    if (NULL == file_name)
+    {
+        return EXIT_FAILURE;
+    }
     file.open(file_name);
     return itpp::exist(file_name)?EXIT_SUCCESS:EXIT_FAILURE;
 }
@@ -16,6 +20,12 @@ static
 int it_file_read_template(const char *const name, T *const data, int *const data_size)
 {
     itpp::Vec<T> out;
+
+    if ((NULL == name) || (NULL == data) || (NULL == data_size))
+    {
+        return EXIT_FAILURE;
+    }
+
     file >> itpp::Name(name) >> out;
     if (*data_size < out.size())
     {
@@ -31,6 +41,10 @@ template<class T>
 static
 int it_file_write_template(const char *const name, const T *const data, int data_size)
 {
+    if ((NULL == name) || (NULL == data))
+    {
+        return EXIT_FAILURE;
+    }
     itpp::Vec<T> out(data_size);
     memcpy(out._data(), data, data_size*sizeof(*data));
     file << itpp::Name(name) << out;
--- test/test_it_file.c
+++ test/test_it_file.c
@@ -9,15 +9,27 @@
 #define OMEGA (PI/6.0)
 #define FILE_NAME "test_it_file.it"
 #define VAR_NAME "signal"
+#define VAR_NAME_I "signal_i"
 
 int main(void)
 {
     double *signal = malloc(LENGTH*sizeof(*signal));
     double *ref_signal = malloc(LENGTH*sizeof(*ref_signal));
+    int *signal_i = malloc(LENGTH*sizeof(*signal_i));
+    int *ref_signal_i = malloc(LENGTH*sizeof(*ref_signal_i));
     int n;
     int out = EXIT_FAILURE;
 
-    if ((NULL == signal) || (NULL == ref_signal))
+    /* check inputs */
+    if (EXIT_FAILURE != it_file_open(NULL))
+    {
+        printf("Should not open file\n");
+        goto fail;
+    }
+
+    /* generate signals */
+    if ((NULL == signal) || (NULL == ref_signal) ||
+            (NULL == signal_i) || (NULL == ref_signal_i))
     {
         printf("Cannot allocate memory\n");
         goto fail;
@@ -25,8 +37,11 @@ int main(void)
     for (n = 0; n < LENGTH; ++n)
     {
         signal[n] = sin(OMEGA*n);
+        signal_i[n] = 0;
     }
+    signal_i[0] = 1;
 
+    /* functional test */
     if (EXIT_SUCCESS != it_file_open(FILE_NAME))
     {
         printf("Cannot open file\n");
@@ -37,7 +52,17 @@ int main(void)
         printf("Cannot write variable\n");
         goto fail;
     }
+    if (EXIT_SUCCESS != it_file_write_int(VAR_NAME_I, signal_i, LENGTH))
+    {
+        printf("Cannot write variable\n");
+        goto fail;
+    }
     if ((EXIT_SUCCESS != it_file_read_double(VAR_NAME, ref_signal, &n)) || (LENGTH != n))
+    {
+        printf("Cannot read variable\n");
+        goto fail;
+    }
+    if ((EXIT_SUCCESS != it_file_read_int(VAR_NAME_I, ref_signal_i, &n)) || (LENGTH != n))
     {
         printf("Cannot read variable\n");
         goto fail;
@@ -49,6 +74,11 @@ int main(void)
             printf("Values don't match\ni");
             goto fail;
         }
+        if (signal_i[n] != ref_signal_i[n])
+        {
+            printf("Values don't match\ni");
+            goto fail;
+        }
     }
 
     printf("SUCCESS\n");
@@ -56,5 +86,7 @@ int main(void)
 fail:
     free(signal);
     free(ref_signal);
+    free(signal_i);
+    free(ref_signal_i);
     return out;
 }
