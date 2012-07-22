e049c27b4071414e258bc38de9f37635f8d179fd
Bogdan Cristea <cristeab@gmail.com>
2012-07-22 21:00:07 +0200

    fix for VS

--- src/it_file.h
+++ src/it_file.h
@@ -1,16 +1,22 @@
 #ifndef IT_FILE_H_
 #define IT_FILE_H_
 
+#ifdef _MSC_VER
+#define DllExport   __declspec( dllexport )
+#else
+#define DllExport
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int it_file_open(const char *const file_name);
-int it_file_read_double(const char *const name, double *const data, int *const data_size);
-int it_file_write_double(const char *const name, const double *const data, int data_size);
-int it_file_read_int(const char *const name, int *const data, int *const data_size);
-int it_file_write_int(const char *const name, const int *const data, int data_size);
-void it_file_close(void);
+DllExport int it_file_open(const char *const file_name);
+DllExport int it_file_read_double(const char *const name, double *const data, int *const data_size);
+DllExport int it_file_write_double(const char *const name, const double *const data, int data_size);
+DllExport int it_file_read_int(const char *const name, int *const data, int *const data_size);
+DllExport int it_file_write_int(const char *const name, const int *const data, int data_size);
+DllExport void it_file_close(void);
 
 #ifdef __cplusplus
 }
